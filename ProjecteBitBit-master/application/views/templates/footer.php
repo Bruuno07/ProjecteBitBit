db5982041089e920c6774bbc5533a937e24d4fea
Bruuno <brunoandia007@gmail.com>
2021-05-11 20:15:24 +0200

    Delete ProjecteBitBit-master directory

--- ProjecteBitBit-master/application/views/templates/footer.php
+++
@@ -1,6 +0,0 @@
-<!-- ini del footer.php -->
-<script src="<?php echo base_url("assets/js/owl.carousel.min.js"); ?>"></script>
-<script src="<?php echo base_url("assets/js/main.js"); ?>"></script>
-
-        </body>
-</html>
